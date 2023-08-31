345fef481c97908331f362a20e3488e119a7f642
Sebastian-is <sebastianmg321@gmail.com>
2023-08-31 10:27:53 -0500

    Update Extract and process

--- Extract.py
+++ Extract.py
@@ -1,29 +1,17 @@
-import boto3
+import json
 import requests
 import datetime
-import os
-import json
+import boto3
 
-print("Starting function execution...")
-# URL de la página a descargar
-url = 'https://www.eltiempo.com'
+url_add = "https://www.eltiempo.com/"
+response = requests.get(url_add)
+html_content = response.text
+fecha = datetime.datetime.now().strftime("%Y-%m-%d")
+pagina =  f'news/raw/contenido-{fecha}.html'
 
-# Obtiene la fecha actual
-current_date = datetime.datetime.now().strftime('%Y-%m-%d')
+bucket_name = "parcial-html"
 
-# Nombre del archivo en S3
-s3_filename = f'news/raw/contenido-{current_date}.html'
-
-# Descarga la página
-response = requests.get(url)
-content = response.text
-
-# Sube el contenido a S3
-bucket_name = os.environ['S3_BUCKET']  # Nombre del bucket de S3
 s3 = boto3.client('s3')
-s3.put_object(Body=content, Bucket=bucket_name, Key=s3_filename)
+s3.put_object(Body=html_content , Bucket=bucket_name, Key=pagina)
 
-latest_filename = {'filename': s3_filename}
-latest_filename_str = json.dumps(latest_filename)
-s3.put_object(Body=latest_filename_str, Bucket=bucket_name, Key='latest-file.json')
-
+print(f"Archivo '{pagina}' subido a '{bucket_name}'")
--- process.py
+++ process.py
@@ -1,41 +1,59 @@
-import os
-import boto3
 from bs4 import BeautifulSoup
-import json
+import boto3
+import datetime
+import csv
+import tempfile
 
-def lambda_handler(event, context):
-    # Obtiene el nombre del archivo más reciente
-    bucket_name = os.environ['S3_BUCKET']
-    s3 = boto3.client('s3')
-    response = s3.get_object(Bucket=bucket_name, Key='latest-file.json')
-    latest_filename_str = response['Body'].read().decode('utf-8')
-    latest_filename = json.loads(latest_filename_str)['filename']
+def obtener(bucket, ruta):
+   s3 = boto3.client('s3')
 
-    # Accede al contenido del archivo HTML más reciente
-    response = s3.get_object(Bucket=bucket_name, Key=latest_filename)
-    html_content = response['Body'].read().decode('utf-8')
+   try:
+      response = s3.get_object(Bucket=bucket, Key=ruta)
+      contenido_html = response['Body'].read().decode('utf-8')
+      return contenido_html
+   except Exception as e:
+      print(f"Error al obtener el archivo: {e}")
+      return None
 
-    # Procesa el contenido HTML usando BeautifulSoup u otras herramientas
-    soup = BeautifulSoup(html_content, 'html.parser')
 
-    # Extrae la información que necesitas (categoría, titular, enlace)
-    category_div = soup.find('div', class_='category-published')
-    category_a = category_div.find('a')
-    category = category_a.text.strip()
+fecha = datetime.datetime.now().strftime("%Y-%m-%d")
+ruta = 'news/raw/contenido-'
+nombre_archivo = f'{fecha}.html' 
+ruta_archivo = f'{ruta}{nombre_archivo}'
+x = obtener('parcial-html' , ruta_archivo)
 
-    headline = ''
-    if soup.find('h1'):
-        headline = soup.find('h1').text.strip()
-    elif soup.find('h2'):
-        headline = soup.find('h2').text.strip()
+generol = []
+titulol = []
+html = x
+soup = BeautifulSoup(html, 'html.parser')
+divs_article_details = soup.find_all('div', class_='article-details')
+for div in divs_article_details:
+  titulo = div.find('a', class_= 'title page-link').text
+  titulol.append(titulo)
+  if div.find('div', class_='category-published') != None:
+    genero = div.find('a').text
+    generol.append(genero)
+  else:
+    genero = "encabezado"
+    generol.append(genero)
 
-    link = ''
-    if headline:
-        link = category_a['href']
+archivo_csv = 'datos.csv'
+with tempfile.NamedTemporaryFile(mode='w', delete=False) as archivo:
+    archivo_csv_temporal = archivo.name
+    escritor_csv = csv.writer(archivo)
+    escritor_csv.writerow(['Titulo', 'Genero'])
+    for titu, gen in zip(titulol, generol):
+        escritor_csv.writerow([titu, gen])
 
-    # Crea el contenido del CSV
-    csv_content = f'{category},{headline},{link}'
+print(f'Se ha creado el archivo CSV: {archivo_csv}')
 
-    # Sube el archivo CSV a S3 en la ruta "headlines/final/"
-    csv_key = 'headlines/final/' + latest_filename.replace("news/raw/contenido-", "").replace(".html", ".csv")
-    s3.put_object(Bucket=bucket_name, Key=csv_key, Body=csv_content.encode('utf-8'))
+fecha_actual = datetime.datetime.now()
+bucket_name = "parcial-csv"
+dia = fecha_actual.day
+mes = fecha_actual.month
+anio = fecha_actual.year
+s3 = boto3.client('s3')
+pagina =  f'headlines/final/periodico/año={anio}/mes={mes}/{fecha}.csv'
+with open(archivo_csv_temporal, 'rb') as archivo:
+   s3.put_object(Body=archivo , Bucket=bucket_name, Key=pagina)
+os.remove(archivo_csv_temporal)
