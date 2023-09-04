8659f0038c633adae39ee29f4a0b1bf56ba150cc
Sebastian-is <129118839+Sebastian-is@users.noreply.github.com>
2023-09-04 09:29:37 -0500

    Update Extract.py

--- Extract.py
+++ Extract.py
@@ -3,28 +3,20 @@ import datetime
 import boto3
 
 
-def obtener_contenido_pagina(url):
-    response = requests.get(url)
-    html_content = response.text
-    return html_content
-
-
-def subir_contenido_a_s3(html_content, bucket_name, key):
-    s3 = boto3.client('s3')
-    s3.put_object(Body=html_content, Bucket=bucket_name, Key=key)
-
-
-def procesar_pagina_web():
+def main():
     url_add = "https://www.eltiempo.com/"
+    response = requests.get(url_add)
+    html_content = response.text
     fecha = datetime.datetime.now().strftime("%Y-%m-%d")
     pagina = f'news/raw/contenido-{fecha}.html'
+
     bucket_name = "parcial-html"
 
-    html_content = obtener_contenido_pagina(url_add)
-    subir_contenido_a_s3(html_content, bucket_name, pagina)
+    s3 = boto3.client('s3')
+    s3.put_object(Body=html_content, Bucket=bucket_name, Key=pagina)
 
     print(f"Archivo '{pagina}' subido a '{bucket_name}'")
 
 
-if __name__ == "__main__":
-    procesar_pagina_web()
+if name == "main":
+    main()
