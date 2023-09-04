886bf2953453ae78cacb9ea46dde5409f2364896
Sebastian-is <129118839+Sebastian-is@users.noreply.github.com>
2023-09-04 09:47:59 -0500

    Update Extract.py

--- Extract.py
+++ Extract.py
@@ -1,4 +1,3 @@
-import json
 import requests
 import datetime
 import boto3
@@ -9,12 +8,12 @@ def main():
     response = requests.get(url_add)
     html_content = response.text
     fecha = datetime.datetime.now().strftime("%Y-%m-%d")
-    pagina =  f'news/raw/contenido-{fecha}.html'
+    pagina = f'news/raw/contenido-{fecha}.html'
 
     bucket_name = "parcial-html"
 
     s3 = boto3.client('s3')
-    s3.put_object(Body=html_content , Bucket=bucket_name, Key=pagina)
+    s3.put_object(Body=html_content, Bucket=bucket_name, Key=pagina)
 
     print(f"Archivo '{pagina}' subido a '{bucket_name}'")
 
