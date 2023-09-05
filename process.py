559c16b18e24b7f5ec704275dba674e8708b8841
Sebastian-is <129118839+Sebastian-is@users.noreply.github.com>
2023-09-04 20:34:40 -0500

    Update process.py

--- process.py
+++ process.py
@@ -5,25 +5,24 @@ import csv
 import tempfile
 import os
 
-
 def obtener(bucket, ruta):
-    s3 = boto3.client('s3')
+   s3 = boto3.client('s3')
 
-    try:
-        response = s3.get_object(Bucket=bucket, Key=ruta)
-        contenido_html = response['Body'].read().decode('utf-8')
-        return contenido_html
-    except Exception as e:
-        print(f"Error al obtener el archivo: {e}")
-        return None
+   try:
+      response = s3.get_object(Bucket=bucket, Key=ruta)
+      contenido_html = response['Body'].read().decode('utf-8')
+      return contenido_html
+   except Exception as e:
+      print(f"Error al obtener el archivo: {e}")
+      return None
 
 
 def main():
     fecha = datetime.datetime.now().strftime("%Y-%m-%d")
     ruta = 'news/raw/contenido-'
-    nombre_archivo = f'{fecha}.html'
+    nombre_archivo = f'{fecha}.html' 
     ruta_archivo = f'{ruta}{nombre_archivo}'
-    x = obtener('parcial-html', ruta_archivo)
+    x = obtener('parcialbg' , ruta_archivo)
 
     generol = []
     titulol = []
@@ -31,14 +30,14 @@ def main():
     soup = BeautifulSoup(html, 'html.parser')
     divs_article_details = soup.find_all('div', class_='article-details')
     for div in divs_article_details:
-        titulo = div.find('a', class_='title page-link').text
+        titulo = div.find('a', class_= 'title page-link').text
         titulol.append(titulo)
-        if div.find('div', class_='category-published') is not None:
-            genero = div.find('a').text
-            generol.append(genero)
-        else:
-            genero = "encabezado"
-            generol.append(genero)
+    if div.find('div', class_='category-published') != None:
+        genero = div.find('a').text
+        generol.append(genero)
+    else:
+        genero = "encabezado"
+        generol.append(genero)
 
     archivo_csv = 'datos.csv'
     with tempfile.NamedTemporaryFile(mode='w', delete=False) as archivo:
@@ -52,14 +51,14 @@ def main():
 
     fecha_actual = datetime.datetime.now()
     bucket_name = "parcialcsv"
+    dia = fecha_actual.day
     mes = fecha_actual.month
     anio = fecha_actual.year
     s3 = boto3.client('s3')
-    pagina = f'headlines/final/periodico/año={anio}/mes={mes}/{fecha}.csv'
+    pagina =  f'headlines/final/periodico/año={anio}/mes={mes}/{fecha}.csv'
     with open(archivo_csv_temporal, 'rb') as archivo:
-        s3.put_object(Body=archivo, Bucket=bucket_name, Key=pagina)
+        s3.put_object(Body=archivo , Bucket=bucket_name, Key=pagina)
     os.remove(archivo_csv_temporal)
 
 
-if __name__ == "__main__":
-    main()
+main()
